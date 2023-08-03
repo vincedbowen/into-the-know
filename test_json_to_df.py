7f8c0d047466fb4018d7421e5bb06f126125fe92
Vincent Bowen <vibo3564@colorado.edu>
2023-08-02 22:23:36 -0600

    fixing imports

--- test_json_to_df.py
+++ test_json_to_df.py
@@ -1,13 +1,14 @@
-from my_app import json_to_dict, dict_to_df
+import my_app
+import back_end 
 import json
 import pandas as pd
 
 def test_empty_json():
     json_file = open('empty_data.json')
     data = json.load(json_file)
-    assert json_to_dict(data) == {}
+    assert back_end.json_to_dict(data) == {}
 
 def test_empty_dict():
     empty_dict = {}
-    empty_df = dict_to_df(empty_dict)
+    empty_df = back_end.dict_to_df(empty_dict)
     assert empty_df.empty == True
