f6f0b8425449ebc8822d8762d59629085eae4b82
Vincent Bowen <vibo3564@colorado.edu>
2023-08-08 15:28:17 -0600

    fixing fatal naming error

--- test_json_to_df.py
+++ test_json_to_df.py
@@ -1,13 +1,13 @@
-import back_end 
+import graph_generator 
 import json
 import pandas as pd
 
 def test_empty_json():
     json_file = open('empty_data.json')
     data = json.load(json_file)
-    assert back_end.json_to_dict(data) == {}
+    assert graph_generator.json_to_dict(data) == {}
 
 def test_empty_dict():
     empty_dict = {}
-    empty_df = back_end.dict_to_df(empty_dict)
+    empty_df = graph_generator.dict_to_df(empty_dict)
     assert empty_df.empty == True
