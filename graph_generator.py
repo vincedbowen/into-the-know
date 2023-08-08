cbece6cf292f0d6b4aec38aa5ad3ee37cf2b2df5
Vincent Bowen <vibo3564@colorado.edu>
2023-08-08 15:16:38 -0600

    docstrings for api call and conversion

--- graph_generator.py
+++ graph_generator.py
@@ -2,10 +2,29 @@ import pandas as pd
 import strava_api
 
 def connect_to_strava():
+    """
+    Connects to the Strava API through urllib3
+
+    Parameters:
+        None
+
+    Returns:
+        connection: an instance of urllib3 Pool Manager 
+    """
     connection = strava_api.connect_to_urllib()
     return connection
 
 def strava_data_for_graph():
+    """
+    Retreives a list of authorized users' activities, and
+    converts json containing activities into a dataframe.
+
+    Parameters:
+        None
+    
+    Returns:
+        activitiy_data_frame: dataframe containing all activities
+    """
     connection = connect_to_strava()
     activity_list = strava_api.get_activities(http = connection)
     activity_dict = json_to_dict(activity_data=activity_list)
@@ -13,6 +32,16 @@ def strava_data_for_graph():
     return activity_data_frame
 
 def json_to_dict(activity_data):
+    """
+    Converts list of acitvities in json form to a 
+    Python dictionary.
+
+    Parameters:
+        activity_data: a json containing list of activities
+    
+    Returns:
+        data_dict: dictionary containing list of activities
+    """
     data_dict = {}
     if not activity_data:
         return {}
@@ -29,6 +58,16 @@ def json_to_dict(activity_data):
     return data_dict
 
 def dict_to_df(dictionary):
+    """
+    Converts a dictionary of authorized user's
+    activities to a dataframe
+
+    Parameters:
+        dictionary: dictionary containing list of activities
+    
+    Returns:
+        data_frame: dataframe containing all activities
+    """
     data_frame= pd.DataFrame.from_dict(dictionary, orient='index')
     data_frame= data_frame.iloc[::-1]
     return data_frame
