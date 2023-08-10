7f3f89a0bef77fc02477a1ea21b997ac3e1d4c95
Vincent Bowen <vibo3564@colorado.edu>
2023-08-09 22:23:56 -0600

    backend of premium zone graph

--- graph_generator.py
+++ graph_generator.py
@@ -26,9 +26,12 @@ def strava_data_for_graph():
         activitiy_data_frame: dataframe containing all activities
     """
     connection = connect_to_strava()
+    bool_premium_user = is_user_premium(connection = connection)
     activity_list = strava_api.get_activities(http = connection)
     activity_dict = json_to_dict(activity_data=activity_list)
     activity_data_frame = dict_to_df(dictionary=activity_dict)
+    if bool_premium_user == True:
+        activity_zone(connection=connection, activity_data_frame=activity_data_frame)
     return activity_data_frame
 
 def json_to_dict(activity_data):
@@ -70,4 +73,60 @@ def dict_to_df(dictionary):
     """
     data_frame= pd.DataFrame.from_dict(dictionary, orient='index')
     data_frame= data_frame.iloc[::-1]
+    data_frame['Heart Rate Zone'] = 0
     return data_frame
+
+def is_user_premium(connection):
+    """
+    Converts a dictionary of authorized user's
+    activities to a dataframe
+
+    Parameters:
+        connection: an instance of urllib3 Pool Manager 
+    
+    Returns:
+        boolean: returns boolean value for if the user is a strava
+            premium member
+    """
+    athlete_json = strava_api.get_athlete(http = connection)
+    if athlete_json['premium'] == True:
+        return True
+    else:
+        return False
+
+def activity_zone(connection, activity_data_frame):
+    """
+    If a user is a premium member, add the zone of their activities to their 
+    activity dataframe
+
+    Parameters:
+        connection: an instance of urllib3 Pool Manager
+        activity_data_frame: dataframe containing id, date, avg watts, max watts
+    
+    Returns:
+        activity_dataframe: returns an updated dataframe with zone data
+    """
+    heart_rate_zones = []
+    break_down_zones = dict()
+    for df_id in activity_data_frame.index:
+        curr_activity_zones = strava_api.get_zones(http=connection, activity_id=df_id)
+        for entry in curr_activity_zones:
+            if entry["type"] == "heartrate":
+                heart_rate_zones = entry["distribution_buckets"]
+        time_max = 0
+        activity_zone = 0
+        for i, zone in enumerate(heart_rate_zones):
+            if zone["max"] != -1 and zone["time"] > time_max:
+                time_max = zone["time"]
+                activity_zone = i
+        #Add one to zone because python is 0-indexed but heartrate zones are not
+        activity_zone += 1
+        activity_data_frame.loc[df_id, ['Heart Rate Zone']] = activity_zone
+    print(activity_data_frame)
+    return activity_data_frame
+
+
+
+
+
+        
